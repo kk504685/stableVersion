232da0c9dda81aae2c0cabe41ee02eb5caaef11c
kk504685 <106187799+kk504685@users.noreply.github.com>
2024-04-15 12:37:46 +0530

    Add files via upload

--- generic.py
+++ generic.py
@@ -2,19 +2,22 @@ import os, json
 from time import sleep
 from dhanhq import dhanhq
 from datetime import datetime
+import ssl, smtplib
+from email.message import EmailMessage
 
 generic_directory = os.path.dirname(os.path.abspath(__file__))
 flag_directory = rf"{generic_directory}\flags"
 generic_script_name = os.path.splitext(os.path.basename(__file__))[0]
-generic_logTime = datetime.now().strftime("%d%m%Y%H%M%S")
-openPriceFlagFile = 'openPrice.flag'
+generic_logTime = datetime.now().strftime("%d%m%Y")
+
+if not os.path.exists(rf"{generic_directory}\logs"):
+    # Create the folder if it doesn't exist
+    os.makedirs(rf"{generic_directory}\logs")
 
 def userDetailsJson():
     with open(rf"{generic_directory}\userDetails.json","r") as config:
         userDetails = json.load(config)
 
-    clientId = userDetails.get('clientId')
-    accessToken = userDetails.get('accessToken')
     processStartTime = userDetails.get('processStartTime')
     processStartTimeHour = int(processStartTime[0:2])
     processStartTimeMinute = int(processStartTime[3:5])
@@ -24,8 +27,6 @@ def userDetailsJson():
     currentTimeHour = datetime.now().hour
     currentTimeMinute = datetime.now().minute
     lotSize = userDetails.get('lotSize')
-    clientIdToPlaceOrder = userDetails.get('clientIdToPlaceOrder')
-    accessTokenToPlaceOrder = userDetails.get('accessTokenToPlaceOrder')
     sleepTime = userDetails.get('sleepTime')
     instrumentToTrade = userDetails.get('instrumentToTrade')
     isPrintEnabled = userDetails.get('enablePrint')
@@ -35,21 +36,21 @@ def userDetailsJson():
     emailReceiver = email['emailReceiver']
     emailPassword = email['emailPassword']
     emailTimeInterval = email['emailTimeInterval']
-    subject = email['subject']
     dataClient = userDetails.get('dataClient')
     orderClient = userDetails.get('orderClient')
     downLoad = userDetails.get('downLoad')
 
     return {
-        "clientId" : clientId, "accessToken" : accessToken, "processStartTime" : processStartTime, "processStartTimeHour" : processStartTimeHour,
+        "processStartTime" : processStartTime, "processStartTimeHour" : processStartTimeHour,
         "processStartTimeMinute" : processStartTimeMinute, "currentTimeHour" : currentTimeHour, "currentTimeMinute" : currentTimeMinute, "lotSize" : lotSize,
-        "clientIdToPlaceOrder" : clientIdToPlaceOrder,"accessTokenToPlaceOrder" : accessTokenToPlaceOrder, "sleepTime" : sleepTime, "instrumentToTrade" : instrumentToTrade,
+        "sleepTime" : sleepTime, "instrumentToTrade" : instrumentToTrade,
         "isPrintEnabled" : isPrintEnabled, "tradeLimitPerInstrument" : tradeLimitPerInstrument, "email" : email, "emailSender" : emailSender, "emailReceiver" : emailReceiver,
-        "emailPassword" : emailPassword, "emailTimeInterval" : emailTimeInterval, "subject" : subject, "dataClient" : dataClient, "orderClient" : orderClient,
+        "emailPassword" : emailPassword, "emailTimeInterval" : emailTimeInterval, "dataClient" : dataClient, "orderClient" : orderClient,
         "downLoad" : downLoad, "processEndTime": processEndTime, "processEndTimeHour" : processEndTimeHour, "processEndTimeMinute" : processEndTimeMinute
         }
 
 userDetails = userDetailsJson()
+sleepTime = userDetails['sleepTime']
 
 dhan = dhanhq(userDetails['dataClient'][0]['clientId'],userDetails['dataClient'][0]['accessToken'])
 
@@ -58,35 +59,7 @@ def genericLog(text):
         log.write(f"{datetime.now()} - {text}\n")
         log.write(f"\n")
 
-def indexOpenPrice(security_id, nickname):
-    global sleepTime
-    genericLog("#################################################################################################################################################")
-    genericLog(rf"Security Id --> {security_id}")
-    openPrice = None
-    while True:
-        try:
-            liveIndexPrice = dhan.intraday_daily_minute_charts(
-                security_id = f"{security_id}",
-                exchange_segment = 'IDX_I',
-                instrument_type = 'INDEX'
-            )
-            if not os.path.exists(rf"{flag_directory}\{openPriceFlagFile}"):
-                with open(rf"{flag_directory}\{openPriceFlagFile}","w") as flag:
-                    openPrice = liveIndexPrice['data']['close'][-1]
-                    json.dump({"openPrice" : openPrice}, flag, indent = 4)
-            else:
-                with open(rf"{flag_directory}\{openPriceFlagFile}","r") as config:
-                    price = json.load(config)
-                    openPrice = price.get('openPrice')
-            genericLog(f"indexOpenPrice() --> {nickname} opened at --> {openPrice}")
-            return openPrice
-        except Exception as e:
-            if userDetails['isPrintEnabled'].lower() == "true":
-                genericLog(rf"indexOpenPrice() --> Error while calling API, will attempt again..")
-                genericLog(e)
-            sleep(sleepTime)
-
-def securityDetails():
+def securityDetails_1():
     current_datetime = datetime.now()
     weekday_number = current_datetime.weekday()
     weekday_name = current_datetime.strftime("%A")
@@ -153,6 +126,100 @@ def securityDetails():
 
     return [security_id, strikePriceGap, nickname, exchangeSegment, instrumentType, unit, quantity]
 
+def securityDetails_2():
+    current_datetime = datetime.now()
+    weekday_number = current_datetime.weekday()
+    weekday_name = current_datetime.strftime("%A")
+
+    if weekday_number == 0:
+        security_id = 442
+        strikePriceGap = 25
+        nickname = "MIDCPNIFTY"
+        exchangeSegment = 'NSE_FNO'
+        instrumentType = 'OPTIDX'
+        unit = 75
+        quantity = userDetails['lotSize'] * unit
+        genericLog(rf"quantity entered by user --> {userDetails['lotSize']} * {unit} = {quantity}")
+        genericLog(rf"{weekday_name} --> {nickname}")
+    elif weekday_number == 1:
+        security_id = 27
+        strikePriceGap = 50
+        nickname = "FINNIFTY"
+        exchangeSegment = 'NSE_FNO'
+        instrumentType = 'OPTIDX'
+        unit = 40
+        quantity = userDetails['lotSize'] * unit
+        genericLog(rf"quantity entered by user --> {userDetails['lotSize']} * {unit} = {quantity}")
+        genericLog(rf"{weekday_name} --> {nickname}")
+    elif weekday_number == 2:
+        security_id = 13
+        strikePriceGap = 50
+        nickname = "NIFTY"
+        exchangeSegment = 'NSE_FNO'
+        instrumentType = 'OPTIDX'
+        unit = 50
+        quantity = userDetails['lotSize'] * unit
+        genericLog(rf"quantity entered by user --> {userDetails['lotSize']} * {unit} = {quantity}")
+        genericLog(rf"{weekday_name} --> {nickname}")
+    elif weekday_number == 3:
+        security_id = 13
+        strikePriceGap = 50
+        nickname = "NIFTY"
+        exchangeSegment = 'NSE_FNO'
+        instrumentType = 'OPTIDX'
+        unit = 50
+        quantity = userDetails['lotSize'] * unit
+        genericLog(rf"quantity entered by user --> {userDetails['lotSize']} * {unit} = {quantity}")
+        genericLog(rf"{weekday_name} --> {nickname}")
+    elif weekday_number == 4:
+        security_id = 51
+        strikePriceGap = 100
+        nickname = "SENSEX"
+        exchangeSegment = 'BSE_FNO'
+        instrumentType = 'OPTIDX'
+        unit = 10
+        quantity = userDetails['lotSize'] * unit
+        genericLog(rf"quantity entered by user --> {userDetails['lotSize']} * {unit} = {quantity}")
+        genericLog(rf"{weekday_name} --> {nickname}")
+    else:
+        genericLog("Market might be closed today, so assigning any security id for testing")
+        security_id = 51
+        strikePriceGap = 100
+        nickname = "sensex_DUMMY"
+        exchangeSegment = 'BSE_FNO'
+        instrumentType = 'OPTIDX'
+        unit = 10
+        quantity = userDetails['lotSize'] * unit
+
+    return [security_id, strikePriceGap, nickname, exchangeSegment, instrumentType, unit, quantity]
+
+def indexOpenPrice(security_id, nickname):
+    global sleepTime
+    genericLog("#################################################################################################################################################")
+    genericLog(rf"Security Id --> {security_id}")
+    openPrice = None
+    while True:
+        try:
+            liveIndexPrice = dhan.intraday_daily_minute_charts(
+                security_id = f"{security_id}",
+                exchange_segment = 'IDX_I',
+                instrument_type = 'INDEX'
+            )
+            if not os.path.exists(rf"{flag_directory}\{nickname}.flag"):
+                with open(rf"{flag_directory}\{nickname}.flag","w") as flag:
+                    openPrice = liveIndexPrice['data']['close'][-1]
+                    json.dump({"openPrice" : openPrice}, flag, indent = 4)
+            else:
+                with open(rf"{flag_directory}\{nickname}.flag","r") as config:
+                    price = json.load(config)
+                    openPrice = price.get('openPrice')
+            genericLog(f"indexOpenPrice() --> {nickname} opened at --> {openPrice}")
+            return openPrice
+        except Exception as e:
+            genericLog(rf"indexOpenPrice() --> Error while calling API, will attempt again..")
+            genericLog(e)
+            sleep(sleepTime)
+
 def apiCall(secId, exchangeSegment, instrumentType):
     try:
         optionPrice = dhan.intraday_daily_minute_charts(
@@ -164,3 +231,20 @@ def apiCall(secId, exchangeSegment, instrumentType):
     except Exception as e:
         print(rf"apiCall() --> Error while calling API, will attempt again..")
         print(e)
+
+def sendEmail(emailSender, emailReceiver, emailPassword, subject):
+    genericLog("sendEmail() --> Initiating mail notification")
+    try:
+        em = EmailMessage()
+        em['From'] = emailSender
+        em['To'] = emailReceiver
+        em['Subject'] = subject
+        #em.set_content(body)
+
+        context = ssl.create_default_context()
+
+        with smtplib.SMTP_SSL('smtp.gmail.com', 465, context = context) as smtp:
+            smtp.login(emailSender, emailPassword)
+            smtp.sendmail(emailSender, emailReceiver, em.as_string())
+    except Exception as e:
+        genericLog("sendEmail() --> Error: {e}")
